149b9c1459212cf1284dcc4133cb8c190869ac40
François Mockers <mockersf@gmail.com>
2020-02-03 21:04:58 +0100

    :alembic: nicer movements

--- game/src/game.rs
+++ game/src/game.rs
@@ -7,6 +7,11 @@ type OwnerNode = Node2D;
 pub struct Game {
     star_scene: Option<PackedScene>,
     player_scene: Option<PackedScene>,
+    player: Player,
+}
+
+struct Player {
+    direction: f32,
 }
 
 unsafe impl Send for Game {}
@@ -17,6 +22,7 @@ impl Game {
         Game {
             star_scene: helpers::load_scene("res://game/StellarObject.tscn"),
             player_scene: helpers::load_scene("res://game/Player.tscn"),
+            player: Player { direction: 0.0 },
         }
     }
 
@@ -49,24 +55,30 @@ impl Game {
     #[export]
     fn _process(&mut self, owner: OwnerNode, delta: f32) {
         let speed = 100.0;
-        let mut movement = euclid::vec2(0.0, 0.0);
+        let angular_speed = 0.05;
+        let mut movement: euclid::Vector2D<f32, euclid::UnknownUnit> = euclid::vec2(0.0, 0.0);
+        let mut rotation = self.player.direction;
         let input = Input::godot_singleton();
         if input.is_action_pressed("ui_right".into()) {
-            movement.x += 1.0;
+            rotation += angular_speed;
         }
         if input.is_action_pressed("ui_left".into()) {
-            movement.x -= 1.0;
+            rotation -= angular_speed;
         }
         if input.is_action_pressed("ui_down".into()) {
-            movement.y += 1.0;
+            movement.x += 1.0;
         }
         if input.is_action_pressed("ui_up".into()) {
-            movement.y -= 1.0;
+            movement.x -= 1.0;
         }
+        self.player.direction = rotation;
+        let rota = euclid::Rotation2D::new(euclid::Angle::radians(rotation));
+        let movement = rota.transform_vector(movement);
         let mut player = unsafe { owner.get_node("player".into()) }
             .and_then(|new_node| unsafe { new_node.cast::<Node2D>() })
             .unwrap();
         unsafe {
+            player.set_rotation(rotation as f64 - std::f64::consts::PI / 2.0);
             player.set_position(player.get_position() + movement * speed * delta);
         }
     }
